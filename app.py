fe33f0aadf6e727ba7e5dc3b9efed6524434006e
chisoo <chisoo@mac.com>
2017-06-29 17:01:57 -0400

    fixed typo in picked_chars.html

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def main():
 def index():
 	if request.method == 'GET':
   		return render_template('index.html')
-  	else: 
-  		return render_template('picked_chars.html')
+	else: 
+		return render_template('picked_chars.html')
   		
 if __name__ == '__main__':
   app.run(port=33507)
