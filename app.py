f5e5ddafc96569173816cbc4659e49899b236651
chisoo <chisoo@mac.com>
2017-06-29 16:55:00 -0400

    Changed back to app.py and added picked_char.html

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 def main():
   return redirect('/index')
 
-@app.route('/index')
+@app.route('/index', methods = ['GET', 'POST'])
 def index():
-  return render_template('index.html')
-
+	if request.method == 'GET':
+  		return render_template('index.html')
+  	else: 
+  		return render_template('picked_chars.html')
+  		
 if __name__ == '__main__':
   app.run(port=33507)
